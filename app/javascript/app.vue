8d0c088c8be60f98c3525674cfed58d1a8d923c9
dock_Imada <gekkouzann334@gmail.com>
2019-11-22 02:34:40 +0000

    js commit

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,59 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>まずはデータを表示</title>
+</head>
+<body>
+
+<div id="app">
+  <p>
+  <input type="text"
+    placeholder="TODOを入力しましょう！"
+    v-model="newItemTitle"
+    v-on:keyup.enter="addTodo">
+  </p>
+
+  <ul>
+    <li v-for="item in items">
+      <label>
+        <input type="checkbox" v-model="item.isChecked"> {{ item.title }}
+      </label>
+    </li>
+  </ul>
+</div>
+
+</body>
+</html>
 </template>
 
 <script>
 export default {
+  el: '#app',
   data: function () {
     return {
-      message: "Hello Vue!"
+      items: [
+          { title: '領収書を準備する', isChecked: true },
+          { title: 'Vue.jsハンズオンの資料を作る', isChecked: true },
+          { title: '参加者の人数を確認する', isChecked: false },
+          { title: 'ピザを注文する', isChecked: false },
+          { title: '参加費のお釣りを準備する', isChecked: false },
+          { title: '会場設営をする', isChecked: false },
+      ],
+      newItemTitle: ''
     }
-  }
+  },
+
+  methods: {
+    addTodo: function(){
+      this.items.push({
+        title: this.newItemTitle,
+        isChecked: false
+      });
+    },
+  },
 }
 </script>
 
